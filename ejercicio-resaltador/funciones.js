c8678c6f17c0ce5cc6ea9c96cf088e253cc1b3ec
fragata-adrian <fragataadrian99@gmail.com>
2020-09-06 22:58:50 -0300

    Ejercicio Resaltador terminado

--- ejercicio-resaltador/funciones.js
+++ ejercicio-resaltador/funciones.js
@@ -2,7 +2,7 @@
 var parrafos = document.querySelectorAll("p");
 
 for (var i=0; i < parrafos.length; i++) {
-    parrafos[i].addEventListener("click", function(e){
-        parrafos[i].classList.toggle("resaltado");
-    })
+    parrafos[i].addEventListener("click", function(){
+        this.classList.toggle("resaltado");
+    });
 }
